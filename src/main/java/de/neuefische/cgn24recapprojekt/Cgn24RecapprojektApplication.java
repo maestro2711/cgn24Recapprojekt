438d53b394a3ad226b674fc72274e0f467ea1bb3
Mitterand <asmitterand@yahoo.fr>
2024-09-16 14:20:29 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/cgn24recapprojekt/Cgn24RecapprojektApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.cgn24recapprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgn24RecapprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cgn24RecapprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/cgn24recapprojekt/Cgn24RecapprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.cgn24recapprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cgn24RecapprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
